d02662fc947c20ad7de33f57b8aa0f42f38cc411
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-11-25 19:03:20 -0500

    Test

---
+++ PPP3/PPP3.cpp
@@ -0,0 +1,64 @@
+// PPP3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//cout << "Please enter your first name (followed by 'enter'):\n";
+	//string first_name; //first_name is a variable of type string
+	//cin >> first_name; //read characters into first_name
+	//cout << "Hello, " << first_name << "!\n";
+
+	//cout << "Please enter your first name and age\n";
+	//string first_name = "???"; //string variable ("???" indicates "don't know the name")
+	//double age = -1; //integer variable (-1 means "don't know the age")
+	//cin >> first_name >> age; //read a string and an integer
+	//cout << "Hello, " << first_name << " (age " << age * 12 << " months old)\n";
+
+	/*cout << "Please enter a floating-point number: ";
+	double n = 0;
+	cin >> n;
+	cout << "n == " << n
+		<< "\nn+1 == " << n + 1
+		<< "\nthree times n == " << 3 * n
+		<< "\ntwice n == " << n + n
+		<< "\nn squared == " << n * n
+		<< "\nhalf of n == " << n / 2
+		<< "\nsquare root of n == " << sqrt(n)
+		<< '\n';*/
+
+	//cout << "Please enter two names\n";
+	//string first;
+	//string second;
+	//cin >> first >> second; // read two strings
+
+	//	if (first == second)
+	//		cout << "That's the same name twice\n";
+	//	if (first < second)
+	//		cout << first << " is alphabetically before " << second << '\n';
+	//	if (first > second)
+	//		cout << first << " is alphabetically after " << second << '\n';
+
+	int number_of_words = 0;
+	string previous;  // previous word; initialized to ""
+	string current;   // current word
+	while (cin >> current) { // read a stream of words
+		number_of_words++;
+		if (previous == current)
+			cout << "word number " << number_of_words << " Repeated word : " << current << '\n';
+		previous = current;
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
